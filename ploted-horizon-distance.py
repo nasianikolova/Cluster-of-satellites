6352324ed9a8464d2c5b47c1f64986d05080526d
nasianikolova <nasia.nikolova@mtel.gr>
2026-02-10 13:05:08 +0200

    plot for horizon distance

---
+++ ploted-horizon-distance.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+R = 1.0
+
+
+h = np.linspace(0, 10000, 10000)  
+
+f = R * np.arccos(R / (R + h))
+
+plt.figure(figsize=(20, 10))
+plt.plot(h, f, label=r"$f(h) = R \arccos\left(\frac{R}{R+h}\right)$")
+
+plt.xlabel("h")
+plt.ylabel("f(h)")
+plt.title("Plot for h ∈ [0, +10000]")
+plt.grid(True)
+plt.legend()
+plt.show()
+
