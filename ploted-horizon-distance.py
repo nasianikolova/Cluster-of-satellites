d025137cb86ac3e49fbe359da84d437f41dd4281
nasianikolova <nasia.nikolova@mtel.gr>
2026-02-24 11:54:13 +0200

    logarithmic plot

--- ploted-horizon-distance.py
+++ ploted-horizon-distance.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-R = 1.0
+R = 6370
 
 
-h = np.linspace(0, 10000, 10000)  
+h = np.geomspace(1e-3, 4e+5, 1000)  
 
 f = R * np.arccos(R / (R + h))
 
 plt.figure(figsize=(20, 10))
-plt.loglog(h, f, label=r"$f(h) = R \arccos\left(\frac{R}{R+h}\right)$")
+plt.semilogx(h, f, label=r"$f(h) = R \arccos\left(\frac{R}{R+h}\right)$")
 
 plt.xlabel("h")
 plt.ylabel("f(h)")
-plt.title("Plot for h ∈ [0, +10000]")
+plt.title("Plot for h ∈ [1e-3, 4e+5]")
 plt.grid(True)
 plt.legend()
 plt.show()
